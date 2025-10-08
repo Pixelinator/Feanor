8264875e838e447c5639ebc1b8cfd03092c18fdc
Dominic <forsterdominic2@gmail.com>
2025-10-08 17:00:17 +0200

    Update flake.nix
    Add deploy-rs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 {
   description = "NixOS + k3s + HeadScale + GitOps + Forgejo + Runner + Ceph";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05",
+    deploy-rs.url = "github:serokell/deploy-rs"
+  };
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./nixos/configuration.nix ];
     };
+
+    deploy.nodes.some-random-system = {
+        hostname = "homeserver";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.homeserver;
+        };
+    };
+
+    # This is highly advised, and will prevent many possible mistakes
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
+
